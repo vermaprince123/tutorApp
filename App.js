9653b82f509ed425707dd5fca26e4200e8dd927b
HeerMehta <mehtaheerh@gmail.com>
2023-05-21 17:07:19 +0530

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, Alert, Image } from 'react-native';
+
+import * as DocumentPicker from 'expo-document-picker';
+import { useState } from 'react';
+import { storage, app } from './firebaseConfig';
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+
 
 export default function App() {
+  const [doc, setDoc] = useState();
+  const [uploading, setUploading] = useState(false);
+  const selectFile = async () => {
+    const doc = await DocumentPicker.getDocumentAsync({});
+    console.log(doc);
+    setDoc(doc);
+    // uploadFile(doc);
+    uploadImageAsync(doc.uri);
+
+  }
+
+  // const uploadFile = async (doc) => {
+  //   setUploading(true);
+  //   const response = await fetch(doc.uri);
+
+  //   const blob = await response.blob();
+  //   const filename = doc.name;
+
+  //   const storageRef = ref(storage, filename);
+
+  //   // 'file' comes from the Blob or File API
+  //   uploadBytes(storageRef, blob).then((snapshot) => {
+  //     console.log('Uploaded a blob or file!');
+  //   }).catch((e) => {console.log(e)});
+  // }
+
+  async function uploadImageAsync(uri) {
+    // Why are we using XMLHttpRequest? See:
+    // https://github.com/expo/expo/issues/2402#issuecomment-443726662
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function (e) {
+        console.log("ERROR");
+        reject(new TypeError("Network request failed"));
+      };
+      xhr.responseType = "blob";
+      xhr.open("GET", uri, true);
+      xhr.send(null);
+      console.log(xhr.open)
+    });
+
+    // console.log(blob, "BLOB")
+
+    const fileRef = ref(storage, '12123');
+    // console.log(fileRef, "FILEREF")
+    const result = await uploadBytes(fileRef, blob);
+
+
+    // We're done with the blob, close and release it
+    blob.close();
+
+    // console.log(result);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text onPress={selectFile}>Select</Text>
+      <Text>{doc && doc.uri}</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ firebaseConfig.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAhfeyjKE5g1f_tuy2_vH-BiHe3L-NQ0lo",
+  authDomain: "test-d7c04.firebaseapp.com",
+  projectId: "test-d7c04",
+  storageBucket: "test-d7c04.appspot.com",
+  messagingSenderId: "849365814185",
+  appId: "1:849365814185:web:447445ecab93e0393712b8",
+  measurementId: "G-RTL2SPQTF8",
+  storageBucket: '',
+//   storageBucket: 'gs://test-d7c04.appspot.com',
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// app.then(() => console.log("done")).catch(() => {console.log("not done")});
+console.log("app done", app);
+const storage = getStorage(app);
+
+console.log(storage, "storage")
+
+export {storage, app};
